4d2dd8f5ae4a094516e76d5ca3a898b49ecde794
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-05-22 12:38:52 -0700

    add function for last 2 of ssn

--- src/bankaccountapp/Account.java
+++ src/bankaccountapp/Account.java
@@ -16,6 +16,15 @@ public abstract class Account implements IBaseRate {
         this.sSN = sSn;
         this.balance = initDeposit;
         System.out.println("NAME: " + name + "\nSSN: " + sSn + "\nBALANCE: $" + balance);
+
+        // set account number
+        this.accountNumber = setAccountNumber();
+        System.out.println("ACCOUNT NUMBER " + this.accountNumber);
+    }
+
+    private String setAccountNumber(){
+        String lastTwoOfSSN = sSN.substring(sSN.length()-2);
+        return lastTwoOfSSN;
     }
 
     // List common methods
