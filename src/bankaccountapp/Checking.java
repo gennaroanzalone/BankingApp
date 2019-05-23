7595e0b7eadb9196e1a017ae268fbbb8f45c10a7
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-05-23 11:04:12 -0700

    add debit card number and pin

--- src/bankaccountapp/Checking.java
+++ src/bankaccountapp/Checking.java
@@ -10,11 +10,21 @@ public class Checking extends Account {
     public Checking(String name, String sSn, double initDeposit) {
         super(name, sSn, initDeposit);
         accountNumber = "2" + accountNumber;
+        setDebitCard();
+    }
+
+    private void setDebitCard(){
+        debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+        debitCardPin = (int) (Math.random() * Math.pow(10, 4));
     }
 
     // List any methods specific to the checking account
     public void showInfo(){
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: CHECKING");
+        System.out.println(
+                " Your Checking Account Details: " +
+                "\n Debit Card Number: " + debitCardNumber +
+                " \n Debit Card Pin: " + debitCardPin
+        );
     }
 }
