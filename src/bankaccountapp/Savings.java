03f69dda6a34505724144dcb3f6984a500ce5f74
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-05-23 10:49:38 -0700

    add function to create safety deposit box id and key

--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -3,16 +3,26 @@ package bankaccountapp;
 public class Savings extends Account {
     // List properties specific to savings account
     int safetyDepositBoxID;
-    int getSafetyDepositBoxKey;
+    int SafetyDepositBoxKey;
 
     // Constructor to initialize setting for the savings properties
     public Savings(String name, String sSn, double initDeposit) {
         super(name, sSn, initDeposit);
         accountNumber = "1" + accountNumber;
+        setSafetyDepositBox();
+    }
+
+    private void setSafetyDepositBox(){
+        safetyDepositBoxID = (int) (Math.random() * Math.pow(10, 3));
+        SafetyDepositBoxKey = (int) (Math.random() * Math.pow(10, 4));
     }
     // list any methods specific to savings account
     public void showInfo(){
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: SAVINGS");
+        System.out.println(
+                " Your Savings Account Features" +
+                "\n Safety Deposit Box ID: " + safetyDepositBoxID +
+                "\n Safety Deposit Box Key: " + SafetyDepositBoxKey
+        );
     }
 }
