e0cfbb3c862d5a15cfa966584d440a94cf203260
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-06-04 15:02:14 -0700

    remove commented out code

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -5,18 +5,7 @@ import java.util.List;
 public class BankAccountApp {
     public static void main(String[] args) {
         String file = "/Users/gnnro/Desktop/NewBankAccounts.csv";
-
-
-
-//        Checking chkacc1 = new Checking("Tom Wilson", "123456789", 1500);
-//        Savings savacc1 = new Savings("Rich Lowe", "987654321", 2500);
-//
-//        savacc1.compound();
-//
-//        savacc1.showInfo();
-//        System.out.println("**********************");
-//        chkacc1.showInfo();
-
+        
 
         // Read a CSV file then create new account based on that data
         List<String[]> newAccountHolders = utilities.csv.read(file);
