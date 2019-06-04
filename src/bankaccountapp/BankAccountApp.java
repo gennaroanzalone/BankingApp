75b6e57ed775b3aadafb3cc6b96e98333c820619
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-06-04 11:12:40 -0700

    fix spacing

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -4,15 +4,12 @@ public class BankAccountApp {
     public static void main(String[] args) {
 
         Checking chkacc1 = new Checking("Tom Wilson", "123456789", 1500);
-
         Savings savacc1 = new Savings("Rich Lowe", "987654321", 2500);
 
         savacc1.compound();
 
         savacc1.showInfo();
-
         System.out.println("**********************");
-
         chkacc1.showInfo();
 
 
