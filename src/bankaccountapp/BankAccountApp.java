7d2902272c261b6bc3d8013c9827206802791ca8
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-06-04 15:32:13 -0700

    move location of read file. Add function to add accounts

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,13 +1,15 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
     public static void main(String[] args) {
-        String file = "/Users/gnnro/Desktop/NewBankAccounts.csv";
-        
+        List<Account> accounts = new LinkedList<Account>();
+
 
         // Read a CSV file then create new account based on that data
+        String file = "/Users/gnnro/Desktop/NewBankAccounts.csv";
         List<String[]> newAccountHolders = utilities.csv.read(file);
         for (String[] accountHolder : newAccountHolders) {
             String name = accountHolder[0];
@@ -16,12 +18,19 @@ public class BankAccountApp {
             double initDeposit = Double.parseDouble(accountHolder[3]);
 //            System.out.println(name + " " + ssn + " " + accountType + " $" + initDeposit);
             if (accountType.equals("Savings")) {
-                System.out.println("OPEN A SAVINGS ACCOUNT");
-            } else if (accountType.equals("Checking")) {
-                System.out.println("OPEN A CHECKING ACCOUNT");
-            } else {
+                accounts.add(new Savings(name, ssn, initDeposit));
+            }
+            else if (accountType.equals("Checking")) {
+                accounts.add(new Checking(name, ssn, initDeposit));
+            }
+            else {
                 System.out.println("ERROR READING ACCOUNT TYPE");
             }
+        }
+
+        for (Account acc : accounts) {
+            System.out.println("\n******************");
+            acc.showInfo();
         }
     }
 }
